1c3502b52d673841a3f5dfd93bd50bfbb938e3ac
irons163 <irons@ironsdeMacBook-Air.local>
2015-09-04 19:05:41 +0800

    Initial Commit

---
+++ Try_Split_Blade/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Try_Split_Blade
+//
+
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Try_Split_Blade/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Try_Split_Blade
+//
+
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
